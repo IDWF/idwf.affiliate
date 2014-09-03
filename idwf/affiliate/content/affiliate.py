812f2ff502ec046ffbd4199048ec8a4ab124bcb2
Khairil Yusof <khairil.yusof@gmail.com>
2014-09-03 10:00:39 +0300

    Remove required for all fields, except Title and Description, as most affliates have no details.

--- idwf/affiliate/content/affiliate.py
+++ idwf/affiliate/content/affiliate.py
@@ -39,38 +39,49 @@ class IAffiliate(form.Schema, IImageScaleTraversable):
                               description=u'Brief description '
                               'or summary of organization.')
     objectives = RichText(title=u'Objectives, Aims, Mission & '
-                          'Vision of Organization',)
-    history = RichText(title=u'History of Organization')
+                          'Vision of Organization',
+                          required=False,)
+    history = RichText(title=u'History of Organization',required=False)
 
     #Contact Details
-    street_address = schema.TextLine(title=u'Street Adress')
+    street_address = schema.TextLine(
+            title=u'Street Adress',required=False)
     phone_number = schema.TextLine(title=u'Phone Number',
                                    description=u'Office No. eg. '
-                                   '+1 34 4444 4444')
+                                   '+1 34 4444 4444',
+                                   required=False)
     fax_number = schema.TextLine(title=u'Fax Number',
-                                 description=u'Fax No. eg. +1 34 4444 4444')
-    email = schema.TextLine(title=u'Email address')
+                         description=u'Fax No. eg. +1 34 4444 4444',
+                                 required=False)
+    email = schema.TextLine(title=u'Email address',required=False)
     website_url = schema.TextLine(title=u'Website URL',
-                                  description=u'eg. https://www.idwfed.org')
+                            description=u'eg. https://www.idwfed.org',
+                            required=False,)
     #Number of Members
-    members_male_no = schema.Int(title=u'Number of Male Members')
-    members_female_no = schema.Int(title=u'Number of Female Members')
+    members_male_no = schema.Int(
+                title=u'Number of Male Members',required=False)
+    members_female_no = schema.Int(title=u'Number of Female Members',
+                                    required=False,)
     phone_number = schema.TextLine(title=u'Phone Number',
                                    description=u'Office No. eg. +1 '
-                                   '34 4444 4444')
+                                   '34 4444 4444', required=False)
 
     #Membership Details
-    member_fees = schema.Bool(title=u'Check if members pay fees')
+    member_fees = schema.Bool(title=u'Check if members pay fees',
+                                required=False,)
     maintains_register_fees = schema.Bool(
-        title=u'Maintains register of fees paid')
-    year_established = schema.Date(title=u'Year Established')
+        title=u'Maintains register of fees paid',
+        required=False)
+    year_established = schema.Date(title=u'Year Established',
+                        required=False)
 
     organization_type = schema.Choice(
            title=_(u'Organization Type'),
            values=[_(u'Trade Union'),
                    _(u'Worker Association'),
                    _(u'Coop'),
-                   _(u'Other')]
+                   _(u'Other')],
+           required=False,
     )   
 
 alsoProvides(IAffiliate, IFormFieldProvider)
